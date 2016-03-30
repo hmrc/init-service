5b63a75c9a9515ac552e09a5f73108d51fa27d0d
Christopher Townson <iteratoruk@gmail.com>
2016-03-30 15:28:07 +0100

    Simplify build files

--- templates/service/project/MicroServiceBuild.scala
+++ templates/service/project/MicroServiceBuild.scala
@@ -2,21 +2,12 @@ import sbt._
 import uk.gov.hmrc.SbtAutoBuildPlugin
 import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin
 import uk.gov.hmrc.versioning.SbtGitVersioning
+import play.PlayImport._
+import play.core.PlayVersion
 
 object MicroServiceBuild extends Build with MicroService {
 
   val appName = "$!APP_NAME!$"
-
-  override lazy val plugins: Seq[Plugins] = Seq(
-    SbtAutoBuildPlugin, SbtGitVersioning, SbtDistributablesPlugin
-  )
-
-  override lazy val appDependencies: Seq[ModuleID] = AppDependencies()
-}
-
-private object AppDependencies {
-  import play.PlayImport._
-  import play.core.PlayVersion
 
   private val microserviceBootstrapVersion = "$!microserviceBootstrapVersion!$"
   private val playAuthVersion = "$!playAuthVersion!$"
@@ -29,6 +20,12 @@ private object AppDependencies {
   <!--(if MONGO)-->
   private val playReactivemongoVersion = "$!playReactivemongoVersion!$"
   <!--(end)-->
+
+  override lazy val plugins: Seq[Plugins] = Seq(
+    SbtAutoBuildPlugin, SbtGitVersioning, SbtDistributablesPlugin
+  )
+
+  override lazy val appDependencies: Seq[ModuleID] = compile ++ testDependencies ++ itDependencies
 
   val compile = Seq(
     <!--(if MONGO)-->
@@ -45,36 +42,16 @@ private object AppDependencies {
     "uk.gov.hmrc" %% "domain" % domainVersion
   )
 
-  trait TestDependencies {
-    lazy val scope: String = "test"
-    lazy val test : Seq[ModuleID] = ???
-  }
+  val testDependencies: Seq[ModuleID] = baseTestDependencies("test")
 
-  object Test {
-    def apply() = new TestDependencies {
-      override lazy val test = Seq(
-        "uk.gov.hmrc" %% "hmrctest" % hmrcTestVersion % scope,
-        "org.scalatest" %% "scalatest" % "2.2.2" % scope,
-        "org.pegdown" % "pegdown" % "1.4.2" % scope,        
-        "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
-      )
-    }.test
-  }
+  val itDependencies: Seq[ModuleID] = baseTestDependencies("it")
 
-  object IntegrationTest {
-    def apply() = new TestDependencies {
+  private def baseTestDependencies(scope: String): Seq[ModuleID] = Seq(
+    "uk.gov.hmrc" %% "hmrctest" % hmrcTestVersion % scope,
+    "org.scalatest" %% "scalatest" % "2.2.2" % scope,
+    "org.pegdown" % "pegdown" % "1.4.2" % scope,
+    "org.jsoup" % "jsoup" % "1.7.3" % scope,
+    "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
+  )
 
-      override lazy val scope: String = "it"
-
-      override lazy val test = Seq(
-        "uk.gov.hmrc" %% "hmrctest" % hmrcTestVersion % scope,
-        "org.scalatest" %% "scalatest" % "2.2.2" % scope,
-        "org.pegdown" % "pegdown" % "1.4.2" % scope,
-        "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
-      )
-    }.test
-  }
-
-  def apply() = compile ++ Test() ++ IntegrationTest()
 }
-
