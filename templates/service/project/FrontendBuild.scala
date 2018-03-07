e76e76259fd7b11f8b6feb7025ac446631dfbb83
Chris Poole <christopher.poole@digital.hmrc.gov.uk>
2018-03-07 13:54:38 +0000

    Uplifted versions of templates to match platform standard

--- templates/service/project/FrontendBuild.scala
+++ templates/service/project/FrontendBuild.scala
@@ -21,9 +21,9 @@ object FrontendBuild extends Build with MicroService {
 
   def test(scope: String = "test") = Seq(
     "uk.gov.hmrc" %% "hmrctest" % "$!hmrcTestVersion!$" % scope,
-    "org.scalatest" %% "scalatest" % "2.2.6" % scope,
+    "org.scalatest" %% "scalatest" % "3.0.0" % scope,
     "org.pegdown" % "pegdown" % "1.6.0" % scope,
-    "org.jsoup" % "jsoup" % "1.8.1" % scope,
+    "org.jsoup" % "jsoup" % "1.10.2" % scope,
     "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
   )
 
