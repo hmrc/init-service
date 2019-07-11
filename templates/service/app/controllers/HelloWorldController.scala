8414bb5ac7743ca6976905abd342ca3a0cf9a59a
chotai <sunny.chotai@digital.hmrc.gov.uk>
2019-07-11 15:18:35 +0100

    BDOG-325 Upgrade to bootstrap-play-26

---
+++ templates/service/app/controllers/HelloWorldController.scala
@@ -0,0 +1,22 @@
+package uk.gov.hmrc.$!APP_PACKAGE_NAME!$.controllers
+
+import javax.inject.{Inject, Singleton}
+
+import play.api.mvc._
+import uk.gov.hmrc.play.bootstrap.controller.FrontendController
+import uk.gov.hmrc.$!APP_PACKAGE_NAME!$.config.AppConfig
+import uk.gov.hmrc.$!APP_PACKAGE_NAME!$.views.html.hello_world
+
+import scala.concurrent.Future
+
+@Singleton
+class HelloWorldController @Inject()(appConfig: AppConfig, mcc: MessagesControllerComponents)
+    extends FrontendController(mcc) {
+
+  implicit val config: AppConfig = appConfig
+
+  val helloWorld: Action[AnyContent] = Action.async { implicit request =>
+    Future.successful(Ok(hello_world()))
+  }
+
+}
