2f4311f3916ea233304c65a32139cfcc67d83fb6
Tom Wadeson <3607811+tomwadeson@users.noreply.github.com>
2022-03-01 14:59:17 +0000

    BDOG-1540 Metric collection is disabled in tests

--- templates/service/it/HealthEndpointIntegrationSpec.scala
+++ templates/service/it/HealthEndpointIntegrationSpec.scala
@@ -4,6 +4,8 @@ import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatestplus.play.guice.GuiceOneServerPerSuite
+import play.api.Application
+import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.libs.ws.WSClient
 
 class HealthEndpointIntegrationSpec
@@ -15,6 +17,11 @@ class HealthEndpointIntegrationSpec
 
   private val wsClient = app.injector.instanceOf[WSClient]
   private val baseUrl  = s"http://localhost:$port"
+
+  override def fakeApplication(): Application =
+    GuiceApplicationBuilder()
+      .configure("metrics.enabled" -> false)
+      .build()
 
   "service health endpoint" should {
     "respond with 200 status" in {
