dafb03d46b67c4232a912f33ed2f9ef88fd6f961
colin-lamed <9568290+colin-lamed@users.noreply.github.com>
2022-10-07 20:40:56 +0100

    BAU Use variable to keep bootstrap-play (and hmrc-mongo) versions in sync

--- init_service/templates/service/project/AppDependencies.scala
+++ init_service/templates/service/project/AppDependencies.scala
@@ -5,22 +5,25 @@ import sbt._
 
 object AppDependencies {
 
+  private val bootstrapVersion = "$!bootstrapPlayVersion!$"
+  <!--(if MONGO)-->private val hmrcMongoVersion = "$!mongoVersion!$"<!--(end)-->
+
   val compile = Seq(
     <!--(if type=="FRONTEND")-->
-    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28" % "$!bootstrapPlayVersion!$",
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28" % bootstrapVersion,
     "uk.gov.hmrc"             %% "play-frontend-hmrc"         % "$!playFrontendHmrcVersion!$"<!--(if MONGO)-->,<!--(end)-->
     <!--(end)-->
     <!--(if type=="BACKEND")-->
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "$!bootstrapPlayVersion!$"<!--(if MONGO)-->,<!--(end)-->
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % bootstrapVersion<!--(if MONGO)-->,<!--(end)-->
     <!--(end)-->
     <!--(if MONGO)-->
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "$!mongoVersion!$"
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % hmrcMongoVersion
     <!--(end)-->
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "$!bootstrapPlayVersion!$"             % "test, it",
-    <!--(if MONGO)-->"uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % "$!mongoVersion!$"            % Test,<!--(end)-->
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % bootstrapVersion            % "test, it",
+    <!--(if MONGO)-->"uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % hmrcMongoVersion            % Test,<!--(end)-->
     <!--(if type=="FRONTEND")-->
     "org.jsoup"               %  "jsoup"                      % "1.13.1"            % Test,
     <!--(end)-->
